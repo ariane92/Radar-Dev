7c53a5b00449bd4144fbe127aaeef8f8a0039971
ariane92 <ariane.ccomp@gmail.com>
2020-01-16 22:01:23 -0300

    front aplicação

--- Omnistack10/web/src/App.js
+++ Omnistack10/web/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './global.css'
 import './App.css'
 import './Sidebar.css'
+import './Main.css'
 
 //Componente: bloco isolado de HTML, CSS  e JS, o qual não interfere no restante da aplicação
 //Propriedadade: Atributos de componenete - Informação que um componente pai passa para o componente filho 
@@ -13,7 +14,7 @@ function App() {
   return (
     <div id='app'>
       <aside>
-        <strong>Cadastrat</strong>
+        <strong>Cadastrar</strong>
         <form>
           <div class="input-block">
             <label htmlFor="github_username">Usuário GitHub</label>
@@ -41,7 +42,56 @@ function App() {
 
 
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
+              <div className="user-info">
+                <strong>Ariane Mateus</strong>
+                <span>Noje.Js, ReactJS</span>
+              </div>
+            </header>
+            <p>Developer and Data Science enthusiast.</p>
+            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
+          </li>
 
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
+              <div className="user-info">
+                <strong>Ariane Mateus</strong>
+                <span>Noje.Js, ReactJS</span>
+              </div>
+            </header>
+            <p>Developer and Data Science enthusiast.</p>
+            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
+              <div className="user-info">
+                <strong>Ariane Mateus</strong>
+                <span>Noje.Js, ReactJS</span>
+              </div>
+            </header>
+            <p>Developer and Data Science enthusiast.</p>
+            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
+              <div className="user-info">
+                <strong>Ariane Mateus</strong>
+                <span>Noje.Js, ReactJS</span>
+              </div>
+            </header>
+            <p>Developer and Data Science enthusiast.</p>
+            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
+          </li>
+
+        </ul>
       </main>
 
     </div>
