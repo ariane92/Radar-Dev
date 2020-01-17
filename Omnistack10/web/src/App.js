79d24a5ba4d1912fa685dbcb1cae49061f3651db
ariane92 <ariane.ccomp@gmail.com>
2020-01-17 08:19:19 -0300

    Cadastro dev

--- Omnistack10/web/src/App.js
+++ Omnistack10/web/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import api from './services/api'
 import './global.css'
 import './App.css'
 import './Sidebar.css'
@@ -10,92 +10,128 @@ import './Main.css'
 //Estado: informações mantidas pelo componentes (imutabilidade)
 
 function App() {
+  const [devs, setDevs] = useState('')
+
+  const [github_username, setGithubUsername] = useState('')
+  const [techs, setTechs] = useState('')
+
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition (
+      (position) => {
+        const {latitude, longitude} =  position.coords
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
+
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault()
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    })
+
+    setGithubUsername('')
+    setTechs('')
+    
+  }
+
   
   return (
     <div id='app'>
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário GitHub</label>
-            <input name="github_username" id="github_username"  required />
+            <input 
+              name="github_username" 
+              id="github_username"  
+              required 
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}            
+              
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs"  required />
+            <input 
+              name="techs" 
+              id="techs"  
+              required 
+              value={techs}
+              onChange={e => setTechs(e.target.value)}  
+            />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude"  required />
+              <input 
+                type="number"
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude} 
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude"  required />
+              <input 
+                ype="number" 
+                name="longitude" 
+                id="longitude"  
+                required 
+                value={longitude} 
+                onChange={e => setLongitude(e.target.value)}
+              />
            </div>
           </div>
           <button type="submit">Salvar</button>
        </form>
     </aside>
-
-
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
-              <div className="user-info">
-                <strong>Ariane Mateus</strong>
-                <span>Noje.Js, ReactJS</span>
-              </div>
+          {devs.map(dev => (
+            <li className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name}/>
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
             </header>
-            <p>Developer and Data Science enthusiast.</p>
-            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
+            <p>{dev.bio}</p>
+            <a href={`http://github.com/${dev.github_username}`}>Acessar perfil no GitHub</a>
           </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
-              <div className="user-info">
-                <strong>Ariane Mateus</strong>
-                <span>Noje.Js, ReactJS</span>
-              </div>
-            </header>
-            <p>Developer and Data Science enthusiast.</p>
-            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
-              <div className="user-info">
-                <strong>Ariane Mateus</strong>
-                <span>Noje.Js, ReactJS</span>
-              </div>
-            </header>
-            <p>Developer and Data Science enthusiast.</p>
-            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/5742201?s=460&v=4" alt="Ariane Mateus"/>
-              <div className="user-info">
-                <strong>Ariane Mateus</strong>
-                <span>Noje.Js, ReactJS</span>
-              </div>
-            </header>
-            <p>Developer and Data Science enthusiast.</p>
-            <a href="https://github.com/ariane92">Acessar perfil no GitHub</a>
-          </li>
-
+        ))}
         </ul>
       </main>
-
     </div>
-    
   )
 }
 
